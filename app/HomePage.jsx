8937ee99fa3c99e2bfa62c4617023541d2bd575d
Pratyush Poddar <wrick17@gmail.com>
2016-11-21 00:43:10 +0530

    added little more stuff

--- app/HomePage.jsx
+++ app/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router'
 import MinionBlock from './components/MinionBlock.jsx'
 import PacMan from './components/PacMan.jsx'
+import DonkeyKong from './components/DonkeyKong.jsx'
 
 export default class HomePage extends React.Component {
   constructor(props) {
@@ -13,6 +14,10 @@ export default class HomePage extends React.Component {
       <div>
         <MinionBlock />
         <PacMan />
+        <DonkeyKong />
+        <div className="more">
+          And many more to come . . . . . soon
+        </div>
         {this.props.children}
       </div>
     );
---
+++ app/components/DonkeyKong.jsx
@@ -0,0 +1,32 @@
+require("./donkeyKong.less");
+
+import React, {PropTypes} from 'react';
+
+export default class DonkeyKong extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const barrels = [];
+    for (var i = 0; i < 20; i++) {
+      barrels.push(<img className="barrel" src="/images/dbarrel.png" />);
+    }
+    return (
+      <div className="kong-wrapper">
+        <label className="kong-label">
+          <span className="kong-title">Donkey Kong</span>
+          <span className="kong-description">the destroyer</span>
+        </label>
+        <div className="kong">
+          <img className="kong-img" src="/images/donkeyKong.jpg" />
+          {barrels}
+        </div>
+        <img className="ramp" src="/images/ramp.png" />
+      </div>
+    );
+  }
+}
+
+DonkeyKong.propTypes = {
+};
